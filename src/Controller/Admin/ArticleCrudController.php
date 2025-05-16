485e9b5b8234a9a693876da48e8f9227356ff14f
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-05-16 16:45:22 +0200

    Projet

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('titre'),
+            TextEditorField::new('contenu'),
+            AssociationField::new('categories')->autocomplete(),
+        ];
+    }
+    
+}
